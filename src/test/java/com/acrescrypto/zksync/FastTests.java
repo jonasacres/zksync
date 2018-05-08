2d5f7cf9462d0d4acd710b640816c4bb93d917dc
Jonas Acres <jonas@acrescrypto.com>
2018-05-08 07:25:01 -0700

    clean up warnings

--- src/test/java/com/acrescrypto/zksync/FastTests.java
+++ src/test/java/com/acrescrypto/zksync/FastTests.java
@@ -1,16 +1,9 @@
 package com.acrescrypto.zksync;
 
-import java.security.Security;
-
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.slf4j.LoggerFactory;
 
 import com.acrescrypto.zksync.crypto.CryptoTests;
-import com.acrescrypto.zksync.crypto.ch;
 import com.acrescrypto.zksync.fs.localfs.LocalTests;
 import com.acrescrypto.zksync.fs.zkfs.ZKFSTests;
 
