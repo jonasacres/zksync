588dd4456684420987a27c5d041c27b954acc2cb
Jonas Acres <jonas@acrescrypto.com>
2018-05-18 09:38:15 -0700

    break out filesystem tests into own class separate from FastTests

--- src/test/java/com/acrescrypto/zksync/FastTests.java
+++ src/test/java/com/acrescrypto/zksync/FastTests.java
@@ -4,22 +4,16 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.acrescrypto.zksync.crypto.CryptoTests;
-import com.acrescrypto.zksync.fs.backedfs.BackedFSTest;
-import com.acrescrypto.zksync.fs.localfs.LocalFSTests;
-import com.acrescrypto.zksync.fs.ramfs.RAMFSTests;
-import com.acrescrypto.zksync.fs.zkfs.ZKFSTests;
+import com.acrescrypto.zksync.fs.FSTests;
 import com.acrescrypto.zksync.net.NetTests;
 import com.acrescrypto.zksync.utility.UtilityTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	FSTests.class,
 	CryptoTests.class,
-	LocalFSTests.class,
-	ZKFSTests.class,
-	RAMFSTests.class,
 	NetTests.class,
 	UtilityTests.class,
-	BackedFSTest.class
 })
 
 public class FastTests {
---
+++ src/test/java/com/acrescrypto/zksync/fs/FSTests.java
@@ -0,0 +1,22 @@
+package com.acrescrypto.zksync.fs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.acrescrypto.zksync.fs.backedfs.BackedFSTest;
+import com.acrescrypto.zksync.fs.localfs.LocalFSTests;
+import com.acrescrypto.zksync.fs.ramfs.RAMFSTests;
+import com.acrescrypto.zksync.fs.zkfs.ZKFSTests;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	LocalFSTests.class,
+	ZKFSTests.class,
+	RAMFSTests.class,
+	BackedFSTest.class,
+	// SSHFS omitted due to speed issues
+})
+
+public class FSTests {
+
+}
