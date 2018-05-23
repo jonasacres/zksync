f67c28d5d6d97ec7339666e3dfb5de001e83c1b1
Jonas Acres <acresjonas@gmail.com>
2018-05-22 19:52:46 -0700

    start TCPPeerSocketListenerTest specs

---
+++ src/test/java/com/acrescrypto/zksync/net/TCPPeerSocketListenerTest.java
@@ -0,0 +1,11 @@
+package com.acrescrypto.zksync.net;
+
+public class TCPPeerSocketListenerTest {
+	// begins listening when constructor called
+	// listens on requested port when specified
+
+	// uses random port when 0 requested
+	// stops listening when close called
+	// getPort returns current port
+	
+}
