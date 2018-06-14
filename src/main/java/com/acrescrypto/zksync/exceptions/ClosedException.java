718a3f4154a7b96d71c03bada00f66ebcabc88af
Jonas Acres <jonas@acrescrypto.com>
2018-06-13 22:19:47 -0700

    more thread litter

---
+++ src/main/java/com/acrescrypto/zksync/exceptions/ClosedException.java
@@ -0,0 +1,12 @@
+package com.acrescrypto.zksync.exceptions;
+
+import java.io.IOException;
+
+public class ClosedException extends IOException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
