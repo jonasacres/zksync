d10fe673c052b3dd28ad3bb4cb8df6b16ca51674
Jonas Acres <jonas@acrescrypto.com>
2019-01-24 09:37:28 -0800

    Change source level to 11 from 1.11. Log exceptions in API.

--- src/main/java/com/acrescrypto/zksyncweb/exceptionmappers/UncaughtExceptionMapper.java
+++ src/main/java/com/acrescrypto/zksyncweb/exceptionmappers/UncaughtExceptionMapper.java
@@ -7,13 +7,17 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.acrescrypto.zksyncweb.data.XAPIResponse;
 
 @Provider
 public class UncaughtExceptionMapper implements ExceptionMapper<Throwable> {
+	private	Logger logger = LoggerFactory.getLogger(UncaughtExceptionMapper.class);
 
-    @Context
-    private HttpHeaders headers;
+	@Context
+	private HttpHeaders headers;
 
 	@Override
 	public Response toResponse(Throwable exception) {
@@ -21,6 +25,8 @@ public class UncaughtExceptionMapper implements ExceptionMapper<Throwable> {
 			return XAPIResponse.withError(404, "Not found").toResponse();
 		}
 		
-    	return XAPIResponse.withError(500, "Server error").toResponse();
+		logger.warn("Caught exception: {}",
+				exception);
+		return XAPIResponse.withError(500, "Server error").toResponse();
 	}
 }
